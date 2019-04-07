12cad81501b12867f4f11efe40a3836ea7c656a6
zeta96 <ahmad.zainul857@gmail.com>
2019-04-07 18:12:19 +0700

    AnyKernel2 :init.spectrum experiment

--- AnyKernel2/ramdisk/init.spectrum.sh
+++ AnyKernel2/ramdisk/init.spectrum.sh
@@ -5,7 +5,31 @@
 # If there is not a persist value, we need to set one
 if [ ! -f /data/property/persist.spectrum.profile ]; then
     setprop persist.spectrum.profile 0
-fi
+fi;
+
+#zram based of ram########################
+if [ $MEM_ALL -gt 3000000000 ]; then
+	ZRAM_SIZE=0;
+elif [ $MEM_ALL -lt 2900000000 ]; then
+	ZRAM_SIZE=1GB;
+else
+	ZRAM_SIZE=512MB;
+fi;
+
+if [ -e /dev/block/zram0 ]; then
+	if [ $MEM_ALL -gt 3000000000 ]; then
+		$BB swapoff /dev/block/zram0 >/dev/null 2>&1;
+		echo "1" > /sys/block/zram0/reset;
+	else
+		$BB swapoff /dev/block/zram0 >/dev/null 2>&1;
+		echo "1" > /sys/block/zram0/reset;
+		echo "lz4" > /sys/block/zram0/comp_algorithm;
+		echo $ZRAM_SIZE > /sys/block/zram0/disksize;
+		$BB mkswap /dev/block/zram0 >/dev/null;
+		$BB swapon /dev/block/zram0;
+	fi;
+fi;
+#################zram#########################
 # Permissions,etc
 BB=/luuvy/busybox
 MEM_ALL=`free | grep Mem | $BB awk '{ print $2 }'`;
@@ -70,29 +94,7 @@ $BB chmod 0444 /sys/devices/system/cpu/$i/cpufreq/stats/*
 done;
 # Cleanup conflicts
 $BB rm -f /vendor/etc/init/init.spectrum.rc;
-#zram based of ram########################
-#if [ $MEM_ALL -gt 3000000000 ]; then
-#	ZRAM_SIZE=0;
-#elif [ $MEM_ALL -lt 2900000000 ]; then
-#	ZRAM_SIZE=1GB;
-#else
-#	ZRAM_SIZE=512MB;
-#fi;
 
-#if [ -e /dev/block/zram0 ]; then
-#	if [ $MEM_ALL -gt 3000000000 ]; then
-#		$BB swapoff /dev/block/zram0 >/dev/null 2>&1;
-#		echo "1" > /sys/block/zram0/reset;
-#	else
-#		$BB swapoff /dev/block/zram0 >/dev/null 2>&1;
-#		echo "1" > /sys/block/zram0/reset;
-#		echo "lz4" > /sys/block/zram0/comp_algorithm;
-#		echo $ZRAM_SIZE > /sys/block/zram0/disksize;
-#		$BB mkswap /dev/block/zram0 >/dev/null;
-#		$BB swapon /dev/block/zram0;
-#	fi;
-#fi;
-#################zram#########################
 # Miscellaneous configs
 echo -n disable > /sys/devices/soc/soc:qcom,bcl/mode;
 
