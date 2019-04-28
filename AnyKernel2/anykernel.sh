c6fb53f926a3a7ac2c9e952e2453518e81d20e7f
zeta96 <ahmad.zainul857@gmail.com>
2019-04-28 20:49:52 +0700

    Anykernel2:anykernel:experiment cp~

--- AnyKernel2/anykernel.sh
+++ AnyKernel2/anykernel.sh
@@ -92,9 +92,13 @@ chmod 644 /system/vendor/etc/init/init.spectrum.sh
 #spectrum write init.rc only
 if [ -e init.rc~ ]; then
 	cp -rpf init.rc~ init.rc
+	###test
+	cp -rpf /system/vendor/etc/init/hw/init.qcom.rc~  /system/vendor/etc/init/hw/init.qcom.rc
+	###
 	if [ -e init.rc ]; then	
-	
-		remove_line /system/vendor/etc/init/hw/init.qcom.rc "import /init.spectrum.rc";
+	#test
+		#remove_line /system/vendor/etc/init/hw/init.qcom.rc "import /init.spectrum.rc";
+		####
 		remove_line /system/vendor/etc/init/hw/init.qcom.rc "import /vendor/etc/init/hw/init.spectrum.rc";
 		remove_line /system/vendor/etc/init/hw/init.qcom.rc "import /vendor/etc/init/init.spectrum.rc";
 		remove_line /system/vendor/etc/init/hw/init.qcom.rc "import /system/etc/init/init.spectrum.rc";
@@ -105,10 +109,7 @@ if [ -e init.rc~ ]; then
 		remove_line init.rc "import /vendor/etc/init/init.spectrum.rc";
 		remove_line init.rc "import /system/etc/init/init.spectrum.rc";
 		backup_file init.rc;
-		insert_line init.rc "init.spectrum.rc" before "import /init.usb.rc" "import /vendor/etc/init/init.spectrum.rc";
-		###experiment if this works
-		insert_line /system/vendor/etc/init/hw/init.qcom.rc "init.spectrum.rc" before "import /vendor/etc/init/hw/init.qcom.usb.rc" "import /vendor/etc/init/init.spectrum.rc";
-		######
+		insert_line init.rc "init.spectrum.rc" before "import /init.usb.rc" "import /vendor/etc/init/init.spectrum.rc";		
 	fi;
 	else
 		if [ -e /system/vendor/etc/init/hw/init.qcom.rc~ ]; then
