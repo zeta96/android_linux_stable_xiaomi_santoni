29597dd99ac233f046f1909e987df9735e512377
zeta96 <ahmad.zainul857@gmail.com>
2019-07-26 15:55:56 +0000

    Revert "xperience:tweak with uc"
    This reverts commit 2d52f34cadbc29849783af2d5bfacbc3b7e3fb38.

--- drivers/cpufreq/cpufreq_xperience.c
+++ drivers/cpufreq/cpufreq_xperience.c
@@ -40,7 +40,7 @@
  * towards the ideal frequency and slower after it has passed it. Similarly,
  * lowering the frequency towards the ideal frequency is faster than below it.
  */
-#define DEFAULT_AWAKE_IDEAL_FREQ 768000
+#define DEFAULT_AWAKE_IDEAL_FREQ 902400
 static unsigned int awake_ideal_freq;
 
 /*
@@ -49,7 +49,7 @@ static unsigned int awake_ideal_freq;
  * that practically when sleep_ideal_freq==0 the awake_ideal_freq is used
  * also when suspended).
  */
-#define DEFAULT_SLEEP_IDEAL_FREQ 345600 /*I Think 300mhz are good :D*/
+#define DEFAULT_SLEEP_IDEAL_FREQ 768000 /*I Think 300mhz are good :D*/
 static unsigned int sleep_ideal_freq;
 
 /*
